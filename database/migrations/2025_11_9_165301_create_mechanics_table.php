84233cc628da76c59e9f5f60c2ed2f709a85f621
daniee010 <tarhembe10@gmail.com>
2025-11-18 11:59:42 -0500

    db mod2

--- database/migrations/2025_11_9_165301_create_mechanics_table.php
+++ database/migrations/2025_11_9_165301_create_mechanics_table.php
@@ -13,9 +13,6 @@ return new class extends Migration
     {
         Schema::create('mechanics', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')
-                ->constrained()
-                ->onDelete('cascade');
             $table->string('phone')->nullable();
             $table->string('specialization')->nullable();
             $table->timestamps();
